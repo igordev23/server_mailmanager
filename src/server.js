e0b770dc6b1576c3c6b34cc4281e0e26257989f7
igordev23 <igor.dev89898@gmail.com>
2025-12-05 15:11:08 -0300

    Adccionado portabilidade pro render

--- src/server.js
+++ src/server.js
@@ -5,35 +5,45 @@ import MailtmService from "./services/mailtm.service.js";
 import emailRoutes from "./routes/email.routes.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use("/emails", emailRoutes);
 
-// 🔥 Variável global para guardar email e token
+app.use("/emails", emailRoutes);
+app.use("/mailtm", mailtmRoutes);
+
 export let MAILTM_DATA = null;
 
-app.use("/mailtm", mailtmRoutes);
+async function startServer() {
+const PORT = process.env.PORT || 3000;
 
-// 🔥 Quando o servidor iniciar, cria o email automaticamente
-app.use("/mailtm", mailtmRoutes);
+app.listen(PORT, async () => {
+console.log("Servidor rodando na porta " + PORT);
 
-app.listen(3000, async () => {
-  console.log("Servidor rodando na porta 3000 🚀");
+MAILTM_DATA = await MailtmService.createMailTmAccount();
+console.log("Conta criada:");
+console.log("Email:", MAILTM_DATA.email);
+console.log("Token:", MAILTM_DATA.token);
 
-  MAILTM_DATA = await MailtmService.createMailTmAccount();
-  console.log("Conta criada:", MAILTM_DATA);
-  console.log("📧 Email:", MAILTM_DATA.email);
-  console.log("🔑 Token:", MAILTM_DATA.token);
-  // 🔥 Iniciar sincronização automática a cada 10 segundos
+const baseUrl = process.env.RENDER_EXTERNAL_URL;
+if (!baseUrl) {
+  console.log("A variável RENDER_EXTERNAL_URL não está definida.");
+  return;
+}
+
 setInterval(async () => {
   try {
-    console.log("⏱ Rodando sync automático...");
-    const response = await fetch("http://localhost:3000/mailtm/sync");
+    console.log("Rodando sync automático...");
+    const response = await fetch(baseUrl + "/mailtm/sync");
     const json = await response.json();
     console.log("Resultado:", json);
   } catch (err) {
-    console.error("Erro no sync automático:", err);
+    console.error("Erro no sync:", err);
   }
-}, 10000); // 10 segundos
+}, 10000);
+
 
 });
+}
+
+startServer();
